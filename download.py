104d07a469c907ca1e40895b37ecf42ffebf1787
Kiran <k@localhost.localdomain>
2021-03-24 14:55:05 +0000

    fetching total cves initiall

--- download.py
+++ download.py
@@ -2,11 +2,12 @@ import requests
 import json
 
 headers = {'Content-Type': 'application/json'}
-pagesize=2000
+pagesize=2000 #Even though documentation specifies 5000 is the maximum, this is the largest size i can get working
 
+initial_response=requests.get("https://services.nvd.nist.gov/rest/json/cves/1.0?resultsPerPage=1",headers=headers)
+initial_cves=json.loads(response.content.decode('utf-8'))
+total_cves=initial_cves'totalResults']
 response=requests.get("https://services.nvd.nist.gov/rest/json/cves/1.0?resultsPerPage={}".format(pagesize),headers=headers)
 
-intial_cves=json.loads(response.content.decode('utf-8'))
+print (intial_cves['totalResults'])
 
-print(response)
-print (intial_cves)
